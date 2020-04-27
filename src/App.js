8fd9c4b07d1fc22c42575d0a4ea0211dd014d590
Assra Anjum <assraanjum287@gmail.com>
2020-04-27 12:57:48 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Style from './component/Style';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Style/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Style/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Title = styled.div`
+    font-size:  1.5rem;
+    color: palevioletred;
+    padding:1em 5em;
+`;
+const Wrapper = styled.div`
+    padding:0em 20em;
+    display:block;
+    
+`;
+const Button = styled.div`
+    background: ${props => props.primary ? "palevioletred" : "white" }
+    color: ${props => props.primary ? "white" : "palevioletred"}
+    font-size:1em;
+    margin:0.2em;
+    padding:0.25em 1em;
+    border:2px solid palevioletred;
+    border-radius:3px;
+    display:inline;
+`;
+const TomatoButton = styled(Button)`
+    color : tomato;
+    text-decoration:none;
+`;
+
+const Link = styled.div`
+    display:flex;
+   margin:1em;
+   float:left;
+`;
+const StyledLink = styled(Link)`
+    color:palevioletred;
+    font-weight:bold;
+    display:flex;
+`;
+const Input = styled.input`
+    padding:0.5em;
+    margin:0.5em;
+    color:${props => props.inputColor || "brown" };
+    background:papayawhip;
+    border:none;
+    border-radius:3px;
+`;
+const Thing= styled.div`
+
+`;
+const ReverseButton = props => <Button {...props} children={props.children.split('').reverse()}/>
+const index = () => {
+  return (
+    <Wrapper>
+        <Title>
+            Assra Anjum
+        </Title>
+        <Button as={ReverseButton}>Normal</Button>
+        <Button primary>primary</Button>
+        <TomatoButton as="a" href="https://www.google.com/">Tomato Button</TomatoButton>
+        <Link>Unstyled, Boring Link</Link>
+        <StyledLink>Styled, exciting Link</StyledLink>
+        <Input defaultValue="Amyra" type="text" />
+        <Input defaultValue="Assra" type="text" inputColor="red"/>
+        <Thing>Hello World!</Thing>
+        <Thing>Hello World!</Thing>
+        <Thing className="something">Hello World!</Thing>
+        <Thing>Hello World!</Thing>
+        <div className="something-else">
+        <Thing>Hello World!</Thing>
+        </div>
+    </Wrapper>
+       
+  
+  );
+}
+
+export default index;
